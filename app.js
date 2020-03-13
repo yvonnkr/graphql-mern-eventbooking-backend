6244c01fd7cc06879caad9f3d16e9e2eda0b4010
Yvonne Nkr <yvonnkr86@gmail.com>
2020-03-13 16:46:46 +0000

    express-graphql,playground & basic initial setup

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+const expressPlayground = require('graphql-playground-middleware-express')
+  .default;
 
 const app = express();
 
 //--alternative to using bodyParser --no need to install bodyParser express covers it
 app.use(express.json({ extended: false }));
+
+//simple schema/resolvers
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!  
+        }
+        type RootMutation {
+            createEvent(name: String): String
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+         }
+    `),
+    rootValue: {
+      events: () => {
+        return ['Romatic Cooking', 'Sailing', 'All-Night Coding'];
+      },
+      createEvent: args => {
+        const eventName = args.name;
+        return eventName;
+      }
+    },
+    graphiql: true //--localhost:5000/graphql
+  })
+);
+
+//alt to graphiql --localhost:5000/playground   --npm i 'graphql-playground-middleware-express' then require as above
+app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
 
 const PORT = process.env.PORT || 5000;
 
